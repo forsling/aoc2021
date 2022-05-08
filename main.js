2f77d62d528291a35247bd578d63285d28574450
Simon Forsling <simon.forsling@gmail.com>
2022-05-08 16:42:44 +0200

    Day 3 Part 2

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ import day1 from './solutions/day1.js';
 //day1();
 
 import day2 from './solutions/day2.js';
-day2();
+//day2();
 
-//import day3 from './solutions/day3.js';
-//day3();
+import day3 from './solutions/day3.js';
+day3();
--- solutions/day3.js
+++ solutions/day3.js
@@ -17,9 +17,10 @@ let exampleInput = [
 ];
 
 let executePart1 = (input) => {   
-    let index = 0;
-    let getNthBitString = (index) => {
-        return input.reduce((prev, curr, ci, arr) => {
+    let maxInputLen = Math.max(...(input.map(el => el.length)));
+
+    let getNthBitString = (inputArr, index) => {
+        return inputArr.reduce((prev, curr) => {
             return prev + curr.charAt(index);
         }, "");
     };
@@ -36,10 +37,9 @@ let executePart1 = (input) => {
         return zeroes > ones ? "0" : "1";
     }
 
-    let maxInputLen = Math.max(...(input.map(el => el.length)));
     let commonBits = "";
     for (let i = 0; i < maxInputLen; i++) {
-        let bitString = getNthBitString(i);
+        let bitString = getNthBitString(input, i);
         commonBits += getCommonBit(bitString);
     }
 
@@ -53,9 +53,56 @@ let executePart1 = (input) => {
     return powerConsumption;
 }
 
+let executePart2 = (input) => {
+    let maxInputLen = Math.max(...(input.map(el => el.length)));
+
+    //Positive for more ones, negative for more zeroes, 0 if they are equal
+    let getCommonBitMeasure = (bitStrings, index) => {
+        let bitnum = 0;
+        for (let line of bitStrings) {
+            let char = line.charAt(index);
+            if (char === "0") {
+                bitnum--;
+            } else if (char === "1") {
+                bitnum++
+            }
+        }
+        return bitnum;
+    };
+
+    let filterArray = (array, charFilter) => {
+        let workingArray = array;
+        for (let i = 0; i < maxInputLen; i++) {
+            let commonBitMeasure = getCommonBitMeasure(workingArray, i);
+            workingArray = workingArray.filter((value) => {
+                return charFilter(value.charAt(i), commonBitMeasure);
+            });
+            if (workingArray.length == 1) {
+                return workingArray[0];
+            }
+        }
+    }
+
+    let oxygenRatingStr = filterArray(input, (char, commonBitMeasure) => {
+        let keepChar = commonBitMeasure >= 0 ? "1" : "0";
+        return char == keepChar;
+    });
+    let oxygenRating = Number.parseInt(oxygenRatingStr, 2);
+
+    let co2ScrubString = filterArray(input, (char, commonBitMeasure) => {
+        let keepChar = commonBitMeasure >= 0 ? "0" : "1";
+        return char == keepChar;
+    });
+    let co2ScrubRating = Number.parseInt(co2ScrubString, 2);
+
+    let lifeSupportRating = oxygenRating * co2ScrubRating;
+    return lifeSupportRating;
+}
 
 export default () => {
     let p1 = executePart1(input);
     console.log("Day 3 Part 1: " + p1);
 
+    let p2 = executePart2(input);
+    console.log("Day 3 Part 2: " + p2);
 }
